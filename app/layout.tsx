1c28cd2f9d2d8fc9d2e03b9ffd402c464ca13887
bvbalaji <balaji_b_v@hotmail.com>
2025-05-18 16:52:57 +0530

    remove unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { Router } from "next/router"
 
-import { Footer } from "@/components/footer"
 import { Header } from "@/components/header"
 
 const inter = Inter({ subsets: ["latin"] })
